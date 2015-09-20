90bde5f9b356a289b7b124a8e6a45e867350c9c0
Hans Hauge <hansthered@gmail.com>
2015-09-20 13:36:30 -0700

    adds a locations page, links to facebook etc

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#show'
+  get '/locations' => 'home#locations'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
